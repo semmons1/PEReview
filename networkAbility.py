440227c106083aeb7b966aa8c9eca58a3f4ff5d5
Connor L Kasarda <47127368+ckasarda@users.noreply.github.com>
2020-03-25 15:00:03 -0600

    Added network ability checking

--- networkAbility.py
+++ networkAbility.py
@@ -1,5 +1,19 @@
-#Potential imports
-def network_Ability(): # -> return string of I.P addresses found, and potential networking capabilities.
-    #stuff here
-    
-    return
+from urlextract import URLExtract
+import re
+# Required modules: idna, uritools, appdirs
+'''
+string -> string possibly containing urls or ip addresses
+'''
+def getNetworkAbility(string):
+    extractor = URLExtract()
+    urlFindings = extractor.find_urls(string)
+    ipFindings = re.findall("(\d{1,3}\.){3}\d{1,3}", string)
+    scorecard = len(urlFindings) + len(ipFindings)
+    #print('Network ability scorecard:', scorecard)
+    #print('Potential urls:', urlFindings)
+    #print('Potential ip addresses:', ipFindings)
+    return scorecard
+
+#network ability examples
+#strings = getSectionStrings("7z.dll", 1, undesirables, False)
+#getNetworkAbility(strings)
