ddccb71b9ef3b62a594fc557980a3d04ce9994eb
Chance McCormick <chancemccormick@outlook.com>
2020-03-24 22:04:32 -0600

    Updated compileTime to potentially work with the changes made.  Have not been able to test, because of errors that occur when I try to run with all of the new implementations

--- compileTime.py
+++ compileTime.py
@@ -1,10 +1,11 @@
 import datetime
 import time
 def compile_Time(portableExe, fileName):  # -> return string of compile time, if not found return "None found."
+    contents = ""
 
     time_Stamp = datetime.datetime.utcfromtimestamp(portableExe.FILE_HEADER.TimeDateStamp)
 
-    print("Compile Time:", time_Stamp)
+    contents += str("<br /> Compile Time:" + time_Stamp + "<br />") 
 
 
-    return
+    return contents
