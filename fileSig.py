894fe253daa47b3142bf15010d46720e0065ebd0
Connor L Kasarda <47127368+ckasarda@users.noreply.github.com>
2020-03-25 18:11:52 -0600

    4 file signature functions

--- fileSig.py
+++ fileSig.py
@@ -1,5 +1,47 @@
-#Potential imports
-def file_Sig(): # -> return string of hash
-    #stuff here
-    
-    return
+import pefile
+
+'''
+portExe -> portable executable in project folder
+sectionNum -> the section number to get hash of
+'''
+def fileSectionSigMD5(portExe, sectionNum):
+    section = portExe.sections[sectionNum]
+    hash = section.get_hash_md5()
+    print('Section name:', section.Name)
+    print('MD5 hash:', hash)
+    return hash
+
+'''
+portExe -> portable executable in project folder
+sectionNum -> the section number to get hash of
+'''
+def fileSectionSigSHA256(portExe, sectionNum):
+    section = portExe.sections[sectionNum]
+    hash = section.get_hash_sha256()
+    print('Section name:', section.Name)
+    print('SHA256 hash: ', hash)
+    return hash
+
+'''
+portExe -> portable executable in project folder
+'''
+def fileSigMD5(portExe):
+    hash = ''
+    for section in portExe.sections:
+        tmpHash = section.get_hash_md5()
+        print('Section name:', section.Name)
+        print('MD5 hash: ', tmpHash)
+        hash += tmpHash
+    return hash
+
+'''
+portExe -> portable executable in project folder
+'''
+def fileSigSHA256(portExe):
+    hash = ''
+    for section in portExe.sections:
+        tmpHash = section.get_hash_sha256()
+        print('Section name:', section.Name)
+        print('SHA256 hash: ', tmpHash)
+        hash += tmpHash
+    return hash
